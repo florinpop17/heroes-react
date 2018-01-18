7c846e5298ede4f410c615efbadca13996b289d2
Florin Pop <popflorin1705@yahoo.com>
2018-01-18 22:29:11 +0200

    stateless functions

--- src/components/Heroes/Heroes.js
+++ src/components/Heroes/Heroes.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Heroes.css';
 
-class Heroes extends Component {
-    render() {
-        const { heroes } = this.props;
-
-        return (
-            <div>
-                <h2>My Heroes</h2>
-                <ul className="heroes">
-                    {heroes && heroes.map(hero => (
-                        <Link to={`/detail/${hero.id}`} key={hero.id}>
-                            <span className="badge">{ hero.id }</span> { hero.name }
-                        </Link>
-                    ))}
-                </ul>
-            </div>
-        );
-    }
-}
+const Heroes = ({ heroes }) => (
+    <div>
+        <h2>My Heroes</h2>
+        <ul className="heroes">
+            {heroes && heroes.map(hero => (
+                <Link to={`/detail/${hero.id}`} key={hero.id}>
+                    <span className="badge">{ hero.id }</span> { hero.name }
+                </Link>
+            ))}
+        </ul>
+    </div>
+);
 
 export default Heroes;
